45da571d770c82aed2f5dd7db40d4d3b8a249aba
Anjali Singh <anjjalisingh14@gmail.com>
2023-11-04 12:31:57 +0530

    user model changed

--- models/User.js
+++ models/User.js
@@ -37,26 +37,30 @@ const userSchema = new mongoose.Schema({
         type: Boolean,
         default: true,
     },
+    institute: {
+        type: String, 
+        required: true,
+    },
     createdAt: {
         type: Date,
         default: Date.now,
     },
 });
 
-// userSchema.methods.generateAuthToken = async function () {
-//     try {
-//         let token = jwt.sign(
-//             { id: this._id, email: this.email },
-//             process.env.SECRET,
-//             {
-//                 expiresIn: '24h',
-//             }
-//         );
+userSchema.methods.generateAuthToken = async function () {
+    try {
+        let token = jwt.sign(
+            { id: this._id, email: this.email },
+            process.env.SECRET,
+            {
+                expiresIn: '24h',
+            }
+        );
 
-//         return token;
-//     } catch (error) {
-//         console.log('Error while generating token');
-//     }
-// };
+        return token;
+    } catch (error) {
+        console.log('Error while generating token');
+    }
+};
 
 export const User = mongoose.model("User", userSchema);
